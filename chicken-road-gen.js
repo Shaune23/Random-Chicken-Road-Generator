d644e98c7c7fddfb155eaffede66f2b149ce79d2
codeMaster23 <gameruzis.15@gmail.com>
2026-02-25 12:44:37 +0200

    Initial commit

---
+++ chicken-road-gen.js
@@ -0,0 +1,9 @@
+// arrays to infitive markers, verbs, prepositions, definitive articles, determinators and Nouns
+
+const infitiveMarker = ['to',]
+const verbs = ['get','run','jump','build','eat','become','think']
+const prepositions = ['in','on','a','at','from','under','over','between','through','with','before','after']
+const definitiveArticle = ['the']
+const determinators = ['the','an','a']
+const nouns = ['city','side','computers','mountain','house','horse','homelander','side','idea','child','okavango','butcher','river']
+
