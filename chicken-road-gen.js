fb82d46338b97d131ed0b1c3b1c677cea16123de
codeMaster23 <gameruzis.15@gmail.com>
2026-02-25 13:28:59 +0200

    randomiser function

--- chicken-road-gen.js
+++ chicken-road-gen.js
@@ -1,9 +1,23 @@
 // arrays to infitive markers, verbs, prepositions, definitive articles, determinators and Nouns
 
-const infitiveMarker = ['to',]
+const infitiveMarker = ['to']
 const verbs = ['get','run','jump','build','eat','become','think']
 const prepositions = ['in','on','a','at','from','under','over','between','through','with','before','after']
 const definitiveArticle = ['the']
 const determinators = ['the','an','a']
 const nouns = ['city','side','computers','mountain','house','horse','homelander','side','idea','child','okavango','butcher','river']
+
+
+// word randomiser
+
+const randomiser =(arr) =>{
+    randomindex =Math.floor(Math.random()* arr.length)
+
+    let randomWord = arr[randomindex]
+    return randomWord
+}
+
+
+
+
 
