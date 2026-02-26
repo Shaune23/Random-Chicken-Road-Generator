ee626414d8ddbf5f228333ecd8107023746a755e
codeMaster23 <gameruzis.15@gmail.com>
2026-02-26 10:06:03 +0200

    final comit

--- chicken-road-gen.js
+++ chicken-road-gen.js
@@ -1,17 +1,17 @@
 // arrays to infitive markers, verbs, prepositions, definitive articles, determinators and Nouns
 
 const infitiveMarker = ['to']
-const verbs = ['get','run','jump','build','eat','become','think']
+const verbs = ['get','run','jump','build','eat','become','think','obliterate','discombobulate','swoop','transfigue','catapult']
 const prepositions = ['in','on','a','at','from','under','over','between','through','with','before','after']
 const definitiveArticle = ['the']
-const determinators = ['the','an','a']
-const nouns = ['city','side','computers','mountain','house','horse','homelander','side','idea','child','okavango','butcher','river']
+
+const nouns = ['city','side','computers','mountain','house','horse','homelander','side','idea','child','okavango','butcher','river','volcano','feather','pocket','storm','engine','shovel']
 
 
 // word randomiser
 
 const randomiser =(arr) =>{
-    randomindex =Math.floor(Math.random()* arr.length)
+    const randomindex =Math.floor(Math.random()* arr.length)
 
     let randomWord = arr[randomindex]
     return randomWord
@@ -19,5 +19,30 @@ const randomiser =(arr) =>{
 
 
 
+// joke maker 
 
 
+
+const makeJoke =()=>{
+    console.log(`${randomiser(infitiveMarker)} ${randomiser(verbs)} ${randomiser(prepositions)} ${randomiser(definitiveArticle)}  ${randomiser(nouns)}`) 
+}
+
+
+
+// joke starter 
+
+
+console.log('Why did the chicken cross the road ?, [type why to find out]')
+
+
+const startJoke =(str)=>{
+
+    if(str === 'why'){
+        return makeJoke()
+
+
+    }
+
+}
+
+startJoke('why')
